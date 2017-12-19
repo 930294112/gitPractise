ba767cc9890a8dd94501540e4d30c894921caf11
shangui120 <15904255407@163.com>
2017-12-19 16:23:51 +0800

    修改代码

--- src/main/java/com/lanou/practise/Test.java
+++ src/main/java/com/lanou/practise/Test.java
@@ -5,6 +5,11 @@ package com.lanou.practise;
  */
 public class Test {
     public static void main(String[] args) {
+
+
+        System.out.println("修改");
+
+
         System.out.println(111);
     }
 }
